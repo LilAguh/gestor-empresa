280b3c00f36bb89c463058d6897fc1b72d95347f
LilAguh <aguezeochoa@gmail.com>
2023-12-31 01:41:43 -0300

    Create a Form file for login registration

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-
 export default function Form(props) {
     return(
-        <div>
-            <form onSubmit={props.handleFormSubmit}>formulario</form>
+        <div className="Form">
+            <h1>Login</h1>
+            <p>Welcome back! Please login to your acount.</p>
+            <form action="">
+                <input type="text" name="mail" placeholder="User Name" required/>
+                <input type="password" name="password" placeholder="Password" required />
+                <button>Login</button>
+            </form>
         </div>
         )
 }
